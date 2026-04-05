1c29174b6e2c4f6db064d5185397a7c176c764c2
L4REN <yuttapong.aoomcub@gmail.com>
2026-04-05 22:41:24 +0700

    Add config.example.php template

---
+++ backend/config.example.php
@@ -0,0 +1,8 @@
+<?php
+// คำแนะนำ: ให้ Copy ไฟล์นี้ แล้วเปลี่ยนชื่อเป็น config.php ก่อนรันระบบ
+// กรุณากรอก DB_Password และ DB_User ตามเครื่องเซิร์ฟเวอร์ของคุณ
+define("DB_Server", 'localhost');
+define("DB_User", 'root');
+define("DB_Password", '');
+define("DB_DatabaseName", 'club_system');
+?>
